0eeece90d6a75dc16434968e8a90f024b0919143
alexvlife <alexvlife@gmail.com>
2018-07-01 00:29:29 +0300

    Vue. Homework by lesson 21/06/2018 - corrected.

--- project_vue/vue_lesson_20180621/js/script.js
+++ project_vue/vue_lesson_20180621/js/script.js
@@ -6,29 +6,34 @@ var vm = new Vue({
             message: 'Скрыть/показать список пользователей',
             userList: [
                 {
+                    id: 1,
                     name: 'Name1',
                     surname: 'Surname1',
                     lastname: 'Lastname1',
                     img: 'img/avatar.png'
                 },
                 {
+                    id: 2,
                     name: 'Name2',
                     surname: 'Surname2',
                     lastname: 'Lastname2',
                     img: 'img/avatar.png'
                 },
                 {
+                    id: 3,
                     name: 'Name3',
                     surname: 'Surname3',
                     lastname: 'Lastname3',
                     img: 'img/avatar.png'
                 },
                 {
+                    id: 4,
                     name: 'Name4',
                     surname: 'Surname4',
                     lastname: 'Lastname4'
                 },
                 {
+                    id: 5,
                     name: 'Name5',
                     surname: 'Surname5',
                     lastname: 'Lastname5'
@@ -38,18 +43,20 @@ var vm = new Vue({
     },
     computed: {
         userListLength: function() {
-          return this.userList.length;
+            return this.userList.length;
         },
         btnText: function() {
-            if(this.show) {
-              return 'Скрыть..';
-            }
-            return 'Показать..';
-          }
+            return this.show ? 'Скрыть..' : 'Показать..';
+        }
+    },
+    methods: {
+        changeShowStatus: function() {
+            this.show = !this.show;
+        }
     },
     filters: {
-      capitalize: function (value) {
-        return value.toUpperCase();
-      }
+        capitalize: function (value) {
+            return value.toUpperCase();
+        }
     }
 });
